b6a263818a2b556e0e152705c5ca9c41a0b73ad9
dummy <aurelienclerac@pm.me>
2023-12-01 15:55:26 +0100

    Part. 2 perf', gitignore

--- Day 1/src/get_next_line.c
+++ Day 1/src/get_next_line.c
@@ -24,31 +24,14 @@ static inline char read_buffer(int fd)
 	return (c);
 }
 
-int get_next_line(int fd, char **line)
+int get_next_line(int fd, char *line)
 {
 	size_t	line_size;
-	size_t	maxi_line;
-	char	*tmp_line;
 	char	c;
 
 	line_size = 0;
-	maxi_line = BUFFER_SIZE;
-	if (fd < 0 || BUFFER_SIZE <= 0 || !line ||
-	(*line = malloc(sizeof(char) * BUFFER_SIZE + 1)) == NULL)
-		return (-1);
 	while (((c = read_buffer(fd)) != '\n') && (c > 0))
-	{
-		(*line)[line_size++] = c;
-		if (line_size == maxi_line)
-		{
-			maxi_line += BUFFER_SIZE;
-			tmp_line = *line;
-			if ((*line = malloc(sizeof(char) * maxi_line + 1)) == NULL)
-				return (-1);
-			ft_strncpy(*line, tmp_line, line_size);
-			free(tmp_line);
-		}
-	}
-	(*line)[line_size] = '\0';
+		line[line_size++] = c;
+	line[line_size] = '\0';
 	return ((c == 0 || c == -1) ? c : line_size);
 }
--- Day 1/src/main.c
+++ Day 1/src/main.c
@@ -18,7 +18,6 @@ static inline bool _is_digit(char c)
 }
 
 /*
-    Poor performance
 
 static inline bool _is_digit(char c)
 {
@@ -33,12 +32,12 @@ static inline bool _is_digit(char c)
 // first part : 54159
 static uint64_t part1(void)
 {
-    char *line;
+    char line[LINE_SIZE];
     int fd = open("input.txt", O_RDONLY);
     size_t i, line_length;
     uint64_t sum = 0;
 
-    while ((line_length = get_next_line(fd, &line)) > 0) {
+    while ((line_length = get_next_line(fd, line)) > 0) {
         i = 0;
         while (__builtin_expect(!_is_digit(line[i]), 1))
             i++;
@@ -49,35 +48,60 @@ static uint64_t part1(void)
             i--;
         sum += line[i] - '0';
     }
-
+    
     close(fd);
     return sum;
 }
 
 static inline int literal_to_digit(const char *str)
 {
-    if      (!strncmp("one", str, 3))   { return 1; } 
-    else if (!strncmp("two", str, 3))   { return 2; }
+    if      (!strncmp("one",   str, 3)) { return 1; } 
+    else if (!strncmp("two",   str, 3)) { return 2; }
     else if (!strncmp("three", str, 5)) { return 3; }
-    else if (!strncmp("four", str, 4))  { return 4; }
-    else if (!strncmp("five", str, 4))  { return 5; }
-    else if (!strncmp("six", str, 3))   { return 6; }
+    else if (!strncmp("four",  str, 4)) { return 4; }
+    else if (!strncmp("five",  str, 4)) { return 5; }
+    else if (!strncmp("six",   str, 3)) { return 6; }
     else if (!strncmp("seven", str, 5)) { return 7; }
     else if (!strncmp("eight", str, 5)) { return 8; }
-    else if (!strncmp("nine", str, 4))  { return 9; }
+    else if (!strncmp("nine",  str, 4)) { return 9; }
     else { return 0; }
 }
+
+/*
+static inline int literal_to_digit(const char *str)
+{
+    switch(str[0]) {
+        case 's': 
+            if      (!strncmp("six",   str, 3))  { return 6; }
+            else if (!strncmp("seven", str, 5))  { return 7; }
+            else                                 { return 0; }
+        case 'f': 
+            if      (!strncmp("four",  str, 4))  { return 4; }
+            else if (!strncmp("five",  str, 4))  { return 5; }
+            else                                 { return 0; }
+        case 't': 
+            if      (!strncmp("two",   str, 3))  { return 2; }
+            else if (!strncmp("three", str, 5))  { return 3; }
+            else                                 { return 0; }
+        case 'e': if (!strncmp("eight", str, 5)) { return 8; } else { return 0; }
+        case 'n': if (!strncmp("nine",  str, 4)) { return 9; } else { return 0; }
+        case 'o': if (!strncmp("one",   str, 3)) { return 1; } else { return 0; }
+        default: return 0;
+    }
+    return 0;
+}
+*/
 
 // second part : 53866
 uint64_t part2(void)
 {
-    char *line;
+    char line[LINE_SIZE];
     int fd = open("input.txt", O_RDONLY);
     uint8_t line_length;
     uint32_t a, b;
     uint64_t sum = 0;
 
-    while ((line_length = get_next_line(fd, &line)) > 0) {
+    while ((line_length = get_next_line(fd, line)) > 0) {
         for (int i = 0; line[i]; i++) {
             if (_is_digit(line[i])) {
                 a = line[i] - '0';
