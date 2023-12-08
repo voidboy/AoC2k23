8e553719d05a7fb7c5e8bc68e2748db3a3b86cce
dummy <aurelienclerac@pm.me>
2023-12-08 18:33:17 +0100

    Day 8, part II !

--- Day 8/src/main.c
+++ Day 8/src/main.c
@@ -58,9 +58,40 @@ struct Pair {
     short c;
 };
 
+struct Lcm {
+    uint64_t v;
+    uint64_t n;
+};
+
+uint64_t max_cycle(
+        const struct Node n[26][26][26],
+        struct Pair *current,
+        const char *directions,
+        size_t i,
+        uint64_t sum
+)
+{
+
+   struct Pair this = *current;
+   if (directions[i] == 'L') {
+       this.a =  n[current->a][current->b][current->c].left[0] - 0x41;
+       this.b =  n[current->a][current->b][current->c].left[1] - 0x41;
+       this.c =  n[current->a][current->b][current->c].left[2] - 0x41;
+   } else if (directions[i] == 'R') {
+       this.a = n[current->a][current->b][current->c].right[0] - 0x41;
+       this.b = n[current->a][current->b][current->c].right[1] - 0x41;
+       this.c = n[current->a][current->b][current->c].right[2] - 0x41;
+   }
+   if (!directions[++i])
+       i = 0;
+   *current = this;
+   if (this.c == 25) {
+       return sum + 1;
+   } else { return max_cycle(n, current, directions, i, sum + 1); }
+}
+
 uint64_t part2(void)
 {
-    uint64_t sum = 0;
     short nn = 0;
     int fd = open("input.txt", O_RDONLY);
     struct Pair start_nodes[512];
@@ -78,39 +109,42 @@ uint64_t part2(void)
         strncpy(n[a][b][c].left,  &( line[7]), 3);
         strncpy(n[a][b][c].right, &(line[12]), 3);
         if (c == 0) { 
-            start_nodes[nn++] = (struct Pair) {
+            start_nodes[nn] = (struct Pair) {
                 .a = a, 
                 .b = b, 
                 .c = c,
             }; 
+            nn++;
         }
+    }
+    struct Lcm lcm[6];
+    for (short i = 0; i < nn; i++) {
+        struct Pair tmp = (struct Pair) {
+            .a = start_nodes[i].a, 
+            .b = start_nodes[i].b, 
+            .c = start_nodes[i].c 
+        };
+        lcm[i].n = lcm[i].v = max_cycle(n, &tmp, directions, 0, 0);
     }
     while (1) {
-        for (short i = 0;; i++, sum++) {
-            if (!directions[i])
-                i = 0;
-            bool end = true;
-            for (short j = 0; j < nn; j++) {
-                const struct Pair current = start_nodes[j];
-                if (directions[i] == 'L') {
-                    start_nodes[j].a =  n[current.a][current.b][current.c].left[0] - 0x41;
-                    start_nodes[j].b =  n[current.a][current.b][current.c].left[1] - 0x41;
-                    start_nodes[j].c =  n[current.a][current.b][current.c].left[2] - 0x41;
-                } else if (directions[i] == 'R') {
-                    start_nodes[j].a = n[current.a][current.b][current.c].right[0] - 0x41;
-                    start_nodes[j].b = n[current.a][current.b][current.c].right[1] - 0x41;
-                    start_nodes[j].c = n[current.a][current.b][current.c].right[2] - 0x41;
-                }
-                if (start_nodes[j].c != 25) { end = false; } 
-            }
-            if (end) { return ++sum; }
+        uint64_t ref = lcm[0].v;
+        short i = 0;
+        for (; i < nn; i++)
+            if (lcm[i].v != ref)
+                break ;
+        if (i == nn) return ref;
+        uint64_t min = 0;
+        for (short i = 0; i < nn; i++) {
+            if (lcm[i].v < lcm[min].v)
+                min = i;
         }
+        lcm[min].v += lcm[min].n;
     }
-    return sum;
+    return 0x1337u;
 }
 
 int main(void)
 {
     assert(part1() == 22199);
-    assert(part2() == 0);
+    assert(part2() == 13334102464297);
 }
