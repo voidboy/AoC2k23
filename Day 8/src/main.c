ddef7643373f9ca4e8048fd931f7702137bcc47f
dummy <aurelienclerac@pm.me>
2023-12-08 15:52:42 +0100

    backup

--- Day 8/src/main.c
+++ Day 8/src/main.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <limits.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <assert.h>
 #include "get_next_line.h"
 #include "split.h"
@@ -33,9 +34,8 @@ uint64_t part1(void)
         strncpy(n[a][b][c].left,  &( line[7]), 3);
         strncpy(n[a][b][c].right, &(line[12]), 3);
     }
-    short i, a, b, c;
-    i = a = b = c = 0;
-    for(; a != 25 && b != 25 && c != 25; i++, sum++) {
+    for(short i = 0, a = 0, b = 0, c = 0;
+        a != 25 && b != 25 && c != 25; i++, sum++) {
         if (!directions[i])
             i = 0;
         const struct Node *current = &(n[a][b][c]);
@@ -52,16 +52,60 @@ uint64_t part1(void)
     return sum;
 }
 
+struct Pair {
+    short a;
+    short b;
+    short c;
+};
+
 uint64_t part2(void)
 {
     uint64_t sum = 0;
+    short nn = 0;
     int fd = open("input.txt", O_RDONLY);
+    struct Pair start_nodes[512];
     char line[LINE_SIZE];
+    char directions[512];
+    struct Node n[26][26][26];
 
-    while (get_next_line(fd, line) > 0) {
-        ;
+    get_next_line(fd, line); // directions
+    strcpy(directions, line);
+    get_next_line(fd, line); // empty
+    for (short a, b, c; get_next_line(fd, line) > 0;) {
+        a = (uint8_t)line[0] - 0x41;
+        b = (uint8_t)line[1] - 0x41;
+        c = (uint8_t)line[2] - 0x41;
+        strncpy(n[a][b][c].left,  &( line[7]), 3);
+        strncpy(n[a][b][c].right, &(line[12]), 3);
+        if (c == 0) { 
+            start_nodes[nn++] = (struct Pair) {
+                .a = a, 
+                .b = b, 
+                .c = c,
+            }; 
+        }
     }
-
+    while (1) {
+        for (short i = 0;; i++, sum++) {
+            if (!directions[i])
+                i = 0;
+            bool end = true;
+            for (short j = 0; j < nn; j++) {
+                const struct Pair current = start_nodes[j];
+                if (directions[i] == 'L') {
+                    start_nodes[j].a =  n[current.a][current.b][current.c].left[0] - 0x41;
+                    start_nodes[j].b =  n[current.a][current.b][current.c].left[1] - 0x41;
+                    start_nodes[j].c =  n[current.a][current.b][current.c].left[2] - 0x41;
+                } else if (directions[i] == 'R') {
+                    start_nodes[j].a = n[current.a][current.b][current.c].right[0] - 0x41;
+                    start_nodes[j].b = n[current.a][current.b][current.c].right[1] - 0x41;
+                    start_nodes[j].c = n[current.a][current.b][current.c].right[2] - 0x41;
+                }
+                if (start_nodes[j].c != 25) { end = false; } 
+            }
+            if (end) { return ++sum; }
+        }
+    }
     return sum;
 }
 
